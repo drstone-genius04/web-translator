7b536375b5c68f0b8b5a8b00a7df86a6dbdb2830
allan <kingallan.10@gmail.com>
2023-08-07 13:45:25 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let user1Messages = [];
+let user2Messages = [];
+
+app.get('/', (req, res) => {
+  let history1 = user1Messages.map(message => `<p>${message[0]}: ${message[1]}</p>`).join('\n');
+  let history2 = user2Messages.map(message => `<p>${message[0]}: ${message[1]}</p>`).join('\n');
+
+  res.send(`
+    <h1>ChatGPT - Translator</h1>
+    <form action="/send/user1" method="post">
+        <label for="user1">User 1:</label><br>
+        <input type="text" id="user1" name="message"><br>
+        <input type="submit" value="User 1 Send">
+    </form>
+    <form action="/send/user2" method="post">
+        <label for="user2">User 2:</label><br>
+        <input type="text" id="user2" name="message"><br>
+        <input type="submit" value="User 2 Send">
+    </form>
+    <h2>Chat History for User 1:</h2>
+    ${history1}
+    <h2>Chat History for User 2:</h2>
+    ${history2}
+  `);
+});
+
+app.post('/send/:user', async (req, res) => {
+  const { user } = req.params;
+  const targetLanguage = user === 'user1' ? 'es' : 'en';
+  const message = req.body.message;
+
+  if (!message || typeof message !== 'string') {
+    return res.status(400).json({ error: 'Invalid message format.' });
+  }
+
+  try {
+    const translatedMessage = await translateMessage(message, targetLanguage);
+
+    if (user === 'user1') {
+      user1Messages.push(['User 1 (English)', message]);
+      user1Messages.push(['ChatGPT (Spanish)', translatedMessage]);
+    } else if (user === 'user2') {
+      user2Messages.push(['User 2 (Spanish)', message]);
+      user2Messages.push(['ChatGPT (English)', translatedMessage]);
+    }
+
+    return res.status(200).json({ translated_message: translatedMessage });
+  } catch (error) {
+    console.error('Translation failed:', error);
+    return res.status(500).json({ error: 'Translation failed. Please try again.' });
+  }
+});
+
+const translateMessage = async (message, targetLanguage) => {
+  const prompt = `Translate the following text to ${targetLanguage}: ${message}`;
+
+  const response = await axios.post('https://api.openai.com/v1/completions', {
+    prompt: prompt,
+    engine: "text-davinci-002",
+    temperature: 0.7,
+    max_tokens: 200
+  }, {
+    headers: {
+      'Authorization': `Bearer your-api-key-here`
+    }
+  });
+
+  const translatedMessage = response.data.choices[0].text.trim();
+  return translatedMessage;
+};
+
+app.listen(3000, () => console.log('Server is running on port 3000.'));
--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Container, TextField, Button, Typography, Box, CircularProgress } from '@material-ui/core';
 
-function App() {
+const App = () => {
+  const [user1Message, setUser1Message] = useState('');
+  const [user2Message, setUser2Message] = useState('');
+  const [chatHistory, setChatHistory] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendMessage = async (user, message, language) => {
+    if (message === '') return;
+    setLoading(true);
+    setError(null);
+    try {
+      const translatedMessage = await translateMessage(message, language);
+
+      const response = await axios.post(`http://localhost:3000/send/${user.toLowerCase()}`, {
+        message: message,
+      });
+
+      if (response.status === 200) {
+        setChatHistory(prevState => [...prevState, { user, message: translatedMessage }]);
+        user === 'User 1' ? setUser1Message('') : setUser2Message('');
+      }
+    } catch (err) {
+      setError('Translation failed. Please try again.');
+    }
+    setLoading(false);
+  };
+
+  const translateMessage = async (message, targetLanguage) => {
+    const prompt = `Translate the following text to ${targetLanguage}: ${message}`;
+    const response = await axios.post('https://api.openai.com/v1/completions', {
+      engine: 'text-davinci-002',
+      prompt: prompt,
+      temperature: 0.7,
+      max_tokens: 200
+    }, {
+      headers: {
+        'Authorization': 'Bearer your-api-key-here'
+      }
+    });
+
+    const translatedMessage = response.data.choices[0].text.trim();
+    return translatedMessage;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Typography variant="h2">ChatGPT - Translator</Typography>
+
+      <TextField
+        label="User 1"
+        variant="outlined"
+        fullWidth
+        value={user1Message}
+        onChange={(e) => setUser1Message(e.target.value)}
+        disabled={loading}
+      />
+      <Button variant="contained" color="primary" onClick={() => sendMessage('User 1', user1Message, 'es')} disabled={loading}>
+        User 1 Send
+      </Button>
+
+      <TextField
+        label="User 2"
+        variant="outlined"
+        fullWidth
+        value={user2Message}
+        onChange={(e) => setUser2Message(e.target.value)}
+        disabled={loading}
+      />
+      <Button variant="contained" color="secondary" onClick={() => sendMessage('User 2', user2Message, 'en')} disabled={loading}>
+        User 2 Send
+      </Button>
+
+      {loading && <CircularProgress />}
+      {error && <Typography color="error">{error}</Typography>}
+
+      <Typography variant="h4">Chat History</Typography>
+      <Box>
+        {chatHistory.map((item, index) => (
+          <Box key={index} m={2}>
+            <Typography variant="h6">{item.user} (Translated):</Typography>
+            <Typography variant="body1">{item.message}</Typography>
+          </Box>
+        ))}
+      </Box>
+    </Container>
   );
-}
+};
 
 export default App;
